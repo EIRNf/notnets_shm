cf8c969d29ff491b353cfd965401879da907deb8
Esteban Ramos <estebanramo@gmail.com>
2023-10-17 22:47:17 -0700

    shm region allocation pass

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "mem.h"
 
 
 int main() {
 
 	std::cout << "Hello World!";
+
+
+	//ALLOCATE MEMORY REGION
+	shared_memory_region test_region(1, 16, create_flag);
+	int shmid = test_region.create();
+	void* shmaddr = test_region.attach(shmid);
+
+
+	//ENQUEUE SOMETHING TO IT
+
+
+	//DEQUEUE SOMETHING TO IT
+
+
+
 	return 0;
 
 }
--- src/mem.h
+++ src/mem.h
@@ -3,33 +3,83 @@
 
 
 //Includes
-
+#include <string>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 //Definitions
+const int create_flag = IPC_CREAT;
+const int only_read_flag = SHM_RDONLY;
 
 class shared_memory_region {
     public:
-        shared_memory_region();
-        void create();
-        void attach();
+        shared_memory_region(key_t key, int size, int shmflg){
+            this->key = key;
+            this->size = size;
+            this->shm_get_flg = shmflg;
+        };
+        int create();
+        void* attach(int shmid);
+        void detach(void* shmaddr);
         void remove();
-        void detach();
 
     private:
+        key_t key;
+        int shmid;
+        void* shmaddr;
+        int size;
+        int shm_get_flg; //IPC_CREAT, IPC_EXCL, SHM_HUGETLB, ...
+    
+    };
 
-};
+int shared_memory_region::create(){
+    int shmid;
+    if ((shmid = shmget(this->key, this->size, this->shm_get_flg)) == -1){
+        perror("shmget: shmget failed");
+        exit(1);
+    } else {
+        this->shmid;
+        return shmid;
+    }
+}
 
+void* shared_memory_region::attach(int shmid){
+    if(this->shmid != shmid){
+        perror("shm attach: attach to different id from  object");
+        exit(1);
+    }
+    void* shmaddr;
+    if ((shmaddr = shmat(shmid, NULL,NULL)) == (void *)-1){
+        perror("shmat: shmat failed");
+        exit(1);
+    } else {
+        this->shmaddr;
+        return shmaddr;
+    }
+}
 
+void shared_memory_region::detach(void* shmaddr){
+    int err;
+    if ((err = shmdt(shmaddr)) ==  -1){
+        perror("shmdt: shmdt failed");
+        exit(1);
+    }
+}
 
+void shared_memory_region::remove(){
+    if (this->shmid == NULL){
+        perror("remove: nothing to remove");
+        exit(1);
+    }
+    int err;
+    if ((err = shmctl(this->shmid, IPC_RMID, NULL)) ==  -1){
+        perror("shmctl: shmctl failed");
+        exit(1);
+    }
+
+}
 
 void HashShmKeys(char* fullServiceName);
-
-
-void* InitializeAndAttachShmRegion();
-void* InitializeShmRegion();
-void* AttachShmRegion();
-void RemoveShmRegion();
-void Detach();
-
 
 #endif
