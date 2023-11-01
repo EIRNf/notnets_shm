79d4f6a2c63be32024ebae121df73d6a1b15ec27
Esteban Ramos <estebanramo@gmail.com>
2023-11-01 14:08:22 -0700

    wip testing coord

--- src/coord.h
+++ src/coord.h
@@ -1,172 +1,177 @@
-// #ifndef __COORD_H
-// #define __COORD_H
+#ifndef __COORD_H
+#define __COORD_H
 
 
-// #include "mem.h"
-// #include "unistd.h"
-// #include <functional>
-// #include <thread>
-// #include <semaphore>
+#include "mem.h"
+#include "unistd.h"
+#include <functional>
+#include <thread>
+#include <semaphore>
 
-// const int available_reserve_slots = 10;
-// const int available_coord_slots = 10;
-
-
-// typedef struct reserve_pair {
-//     bool client_request;
-//     bool server_handled;
-// }reserve_pair; 
-
-// typedef struct key_pair {
-//     int request_shm_key; //Only written to by Server // if -1 then that means it hasnt been created?
-//     int response_shm_key; //Only written to by Server  // if -1 then that means it hasnt been created?
-// } key_pair;
-
-// //Modify to be templatable, permit 
-// typedef struct coord_row {
-//     int client_id; //Only written to by Client, how do we even know id?
-//     bool shm_created;  //Only written to by Server //If false server creates it and adds keys to
-//     key_pair keys;
-//     bool detach;  //Only written to by Client
-// }coord_row;
+const int available_reserve_slots = 10;
+const int available_coord_slots = 10;
 
 
-// typedef struct coord_header {
-// 	int counter;
-// 	// int lock; //Use shared_mutex for now 
-// 	reserve_pair available_slots[available_reserve_slots] ;// This needs to be thread safe
-// 	coord_row coord_slots[available_coord_slots] ;
-// } coord_header;
+typedef struct reserve_pair {
+    bool client_request;
+    bool server_handled;
+}reserve_pair; 
 
-// class coord {
+typedef struct key_pair {
+    int request_shm_key; //Only written to by Server // if -1 then that means it hasnt been created?
+    int response_shm_key; //Only written to by Server  // if -1 then that means it hasnt been created?
+} key_pair;
 
-//     public:
-//         coord();
-//         int create(shared_memory_region *shm);
-//         int attach(shared_memory_region *shm); //doesnt make sense as a call?
-//         int destroy();
+//Modify to be templatable, permit 
+typedef struct coord_row {
+    int client_id; //Only written to by Client, how do we even know id?
+    bool shm_created;  //Only written to by Server //If false server creates it and adds keys to
+    key_pair keys;
+    bool detach;  //Only written to by Client
+}coord_row;
 
-//         int service_requests_poll(std::function<key_pair(int)> allocation, int key_seed); //Continue servicing requests, pass function as desired work to be done. 
-//         key_pair* query_handled_requests();
-//         key_pair try_request_keys(int client_id); //Wait until request is satisfied or timeout
 
-//     private:
-//         shared_memory_region *shm;
-//         std::binary_semaphore sem{0}; //use binary semaphore for now, might be replaced by another
-//         key_pair queue_keys[available_reserve_slots];
+typedef struct coord_header {
+	int counter;
+	// int lock; //Use shared_mutex for now 
+	reserve_pair available_slots[available_reserve_slots] ;// This needs to be thread safe
+	coord_row coord_slots[available_coord_slots] ;
+} coord_header;
 
-//         coord_header* get_coord_header(void* shmaddr);
+class coord {
 
-//         //We want to support varying threading models, how do we surface new allocated(or reused) queues to a user of coord. 
+    public:
+        coord();
+        int create(shared_memory_region *shm);
+        int attach(shared_memory_region *shm); //doesnt make sense as a call?
+        int destroy();
+
+        int service_requests(std::function<key_pair(int)> allocation, int key_seed); //Continue servicing requests, pass function as desired work to be done. 
+        key_pair* query_handled_requests(); //
+        key_pair try_request_keys(int client_id); //Wait until request is satisfied or timeout
+
+    private:
+        shared_memory_region *shm;
+        std::binary_semaphore sem{0}; //use binary semaphore for now, might be replaced by another
+        key_pair queue_keys[available_reserve_slots];
+
+        coord_header* get_coord_header(void* shmaddr);
+
+        //We want to support varying threading models, how do we surface new allocated(or reused) queues to a user of coord. 
         
 
-// };
+};
 
-// coord::coord() {}
+coord::coord() {}
 
-// coord_header* get_coord_header(void* shmaddr){
-//     coord_header* header = (coord_header*)(shmaddr);
-//     return header;
-// }
+coord_header* coord::get_coord_header(void* shmaddr){
+    coord_header* header = (coord_header*)(shmaddr);
+    return header;
+}
 
-// int coord::create(shared_memory_region *shm){
-//     //do check to verify memory region size
-//     if(sizeof(coord_header) > shm->size){
-//         perror("Header data too large for allocated shm");
-//         exit(1);
-//     }
+int coord::create(shared_memory_region *shm){
+    //do check to verify memory region size
+    if(sizeof(coord_header) > shm->size){
+        perror("Header data too large for allocated shm");
+        exit(1);
+    }
 
-//     this->shm = shm;
+    this->shm = shm;
 
-//     coord_header header;
-//     header.counter = 0; 
+    coord_header header;
+    header.counter = 0; 
 
-//     for(int i = 0; i < available_coord_slots; i++){
-//         header.available_slots[i].client_request = false;
-//         header.available_slots[i].server_handled = false;
+    for(int i = 0; i < available_coord_slots; i++){
+        header.available_slots[i].client_request = false;
+        header.available_slots[i].server_handled = false;
 
-//     }
-//     header.coord_slots[available_coord_slots] = { 0 };
+    }
+    header.coord_slots[available_coord_slots] = { 0 };
 
-//     this->queue_keys[available_coord_slots] = {0};
+    this->queue_keys[available_coord_slots] = {0};
 
-//     coord_header *header_ptr = get_coord_header(this->shm->shmaddr);
-//     *header_ptr = header;
-// }
-// int coord::attach(shared_memory_region *shm){
+    coord_header *header_ptr = get_coord_header(this->shm->shmaddr);
+    *header_ptr = header;
+}
+int coord::attach(shared_memory_region *shm){
 
-//     //do check to verify memory region size
-//     if(sizeof(coord_header)> shm->size){
-//         perror("Header data too large for allocated shm");
-//         exit(1);
-//     }
+    //do check to verify memory region size
+    if(sizeof(coord_header)> shm->size){
+        perror("Header data too large for allocated shm");
+        exit(1);
+    }
 
-//     this->shm = shm;
+    this->shm = shm;
 
-//      coord_header *header_ptr = get_coord_header(this->shm->shmaddr);
-// }
-// int coord::destroy(){
+     coord_header *header_ptr = get_coord_header(this->shm->shmaddr);
+}
+int coord::destroy(){
 
-// }
+}
 
-// //Client Section, request a pair of keys for shm
-// key_pair coord::try_request_keys(int client_id){
-//     coord_header *header = this->get_coord_header(shm->shmaddr);
-//     int reserved_slot = -1;
+//Client Section, request a pair of keys for shm
+key_pair coord::try_request_keys(int client_id){
+    coord_header *header = this->get_coord_header(shm->shmaddr);
+    int reserved_slot = -1;
 
-//     ///try to reserve a slot, if not available wait and try again
+    ///try to reserve a slot, if not available wait and try again
 
-//     this->sem.acquire();
-//     for(int i = 0; i <available_coord_slots; i++){
-//         if ((header->available_slots[i].client_request == false) && (header->available_slots[i].server_handled == false) ){
-//             header->available_slots[i].client_request = true;
-//             header->coord_slots[i].client_id = client_id;
-//             header->coord_slots[i].detach = false;
-//             header->coord_slots[i].keys.request_shm_key = -1;
-//             header->coord_slots[i].keys.response_shm_key = -1;
-//             header->coord_slots[i].shm_created = false;
+    this->sem.acquire();
+    for(int i = 0; i <available_coord_slots; i++){
+        if ((header->available_slots[i].client_request == false) && (header->available_slots[i].server_handled == false) ){
+            header->available_slots[i].client_request = true;
+            header->coord_slots[i].client_id = client_id;
+            header->coord_slots[i].detach = false;
+            header->coord_slots[i].keys.request_shm_key = -1;
+            header->coord_slots[i].keys.response_shm_key = -1;
+            header->coord_slots[i].shm_created = false;
 
-//             reserved_slot = -1;
-//         } 
-//     }
-//     this->sem.release();
-//     //Reserve a slot, now polled it until it is handled
-//     //TODO:implement timeout
-//     while(true){
-//         if (header->coord_slots[reserved_slot].shm_created == true){
-//             key_pair keys = {};
-//             keys.request_shm_key =  header->coord_slots[reserved_slot].keys.request_shm_key;
-//             keys.response_shm_key = header->coord_slots[reserved_slot].keys.response_shm_key;
-//             return keys;
-//         }
-//         //tune with wait
-//     }
-// }
+            reserved_slot = -1;
+        } 
+    }
+    this->sem.release();
+    //Reserve a slot, now poll it until it is handled
+    //TODO:implement timeout
+    while(true){
+        if (header->coord_slots[reserved_slot].shm_created == true){
+            key_pair keys = {};
+            keys.request_shm_key =  header->coord_slots[reserved_slot].keys.request_shm_key;
+            keys.response_shm_key = header->coord_slots[reserved_slot].keys.response_shm_key;
+            return keys;
+        }
+        //tune with wait
+    }
+}
 
-// //Server section, handle requests in critical section
-// //Single writer principle, run this with a dedicated thread. 
-// int coord::service_requests_poll(std::function<key_pair(int)> allocation, int key_seed){
-//     coord_header *header = this->get_coord_header(shm->shmaddr);
+//Server section, handle requests in critical section
+//Single writer principle, run this with a dedicated thread. 
+int coord::service_requests(std::function<key_pair(int)> allocation, int key_seed){
+    coord_header *header = this->get_coord_header(shm->shmaddr);
 
-//         ///try to reserve a slot, if not available wait and try again
-//         this->sem.acquire();
-//         for(int i = 0; i <available_coord_slots; i++){
-//             if ((header->available_slots[i].client_request == true) && (header->available_slots[i].server_handled == false) ){
-//                 //Begin handling request, what does this look like?
-//                 //Use passed function that returns keys
+        ///try to reserve a slot, if not available wait and try again
+        this->sem.acquire();
+        for(int i = 0; i <available_coord_slots; i++){
+            if ((header->available_slots[i].client_request == true) && (header->available_slots[i].server_handled == false) ){
+                //Begin handling request, what does this look like?
+                //Use passed function that returns keys
 
-//                 //Call Allocation Function
-//                 key_pair keys = allocation(key_seed);
+                //Call Allocation Function
+                key_pair keys = allocation(key_seed);
                 
-//                 header->coord_slots[i].keys.request_shm_key = keys.request_shm_key;
-//                 header->coord_slots[i].keys.response_shm_key = keys.response_shm_key;
-//                 header->coord_slots[i].shm_created = true;
+                header->coord_slots[i].keys.request_shm_key = keys.request_shm_key;
+                header->coord_slots[i].keys.response_shm_key = keys.response_shm_key;
+                header->coord_slots[i].shm_created = true;
 
-//                 this->queue_keys[i] = keys;
-//                 header->available_slots[i].server_handled = true;
-//             }
-//         }
-//         this->sem.release();
-// }
+                this->queue_keys[i] = keys;
+                header->available_slots[i].server_handled = true;
+            }
+        }
+        this->sem.release();
+}
 
-// #endif
+key_pair* coord::query_handled_requests(){
+    return queue_keys;
+}
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -120,86 +120,98 @@ void data_race_test(){
 
 }
 
-// key_pair coord_test_name_keys(int test){
-// 	int test1 = test+1;
-// 	return key_pair{test, test1};
-// }
+key_pair coord_test_name_keys(int test){
+	int test1 = test+1;
+	return key_pair{test, test1};
+}
 
-// key_pair coord_test_queue_allocation(int key_seed){
-// 	key_pair keys = coord_test_name_keys(key_seed);
+key_pair coord_test_queue_allocation(int key_seed){
+	key_pair keys = coord_test_name_keys(key_seed);
 
-// 	//ALLOCATE MEMORY REGIONS
-// 	shared_memory_region request_test_region(keys.request_shm_key, 200, create_flag);
-// 	shared_memory_region response_test_region(keys.response_shm_key, 200, create_flag);
+	//ALLOCATE MEMORY REGIONS
+	shared_memory_region request_test_region(keys.request_shm_key, 200, create_flag);
+	shared_memory_region response_test_region(keys.response_shm_key, 200, create_flag);
 
-// 	int request_shmid = request_test_region.create();
-// 	void* request_shmaddr = request_test_region.attach(request_shmid);
+	int request_shmid = request_test_region.create();
+	void* request_shmaddr = request_test_region.attach(request_shmid);
 
-// 	int response_shmid = response_test_region.create();
-// 	void* response_shmaddr = response_test_region.attach(response_shmid);
+	int response_shmid = response_test_region.create();
+	void* response_shmaddr = response_test_region.attach(response_shmid);
 
-// 	//Create queue, for proper shared memory usage you would need to "attach" to an existing implementation. Implementing asap
-// 	//Make sure the shared memory is big enough to hold header + atleast one message
-// 	spsc_queue<int> request_queue;
-// 	request_queue.create(&request_test_region);
+	//Create queue, for proper shared memory usage you would need to "attach" to an existing implementation. Implementing asap
+	//Make sure the shared memory is big enough to hold header + atleast one message
+	spsc_queue<int> request_queue;
+	request_queue.create(&request_test_region);
 
-// 	spsc_queue<int> response_queue;
-// 	response_queue.create(&response_test_region);
+	spsc_queue<int> response_queue;
+	response_queue.create(&response_test_region);
 
-// 	return keys;
-// }
+	return keys;
+}
 
 
-// void coord_test(){
+void coord_test(){
 
-// 	//Forking to test multiprocess functionality
-// 	pid_t c_pid = fork(); 
+	//Forking to test multiprocess functionality
+	pid_t c_pid = fork(); 
   
-//     if (c_pid == -1) { 
-//         perror("fork"); 
-//         exit(EXIT_FAILURE); 
-//     } 
-//     else if (c_pid > 0) {  //PARENT PROCESS
+    if (c_pid == -1) { 
+        perror("fork"); 
+        exit(EXIT_FAILURE); 
+    } 
+    else if (c_pid > 0) {  //PARENT PROCESS
 
-// 		shared_memory_region coord_region(1, 200, create_flag);
-// 		coord_region.create();
+		shared_memory_region coord_region(1, 200, create_flag);
+		coord_region.create();
 
-// 		coord test_coord;
-// 		test_coord.create(&coord_region);
+		coord test_coord;
+		test_coord.create(&coord_region);
 
-// 		sleep(2);
-// 		test_coord.service_requests_poll(&coord_test_queue_allocation, 17);
-// 		sleep(2);
+		sleep(2);
+		test_coord.service_requests(&coord_test_queue_allocation, 17);
+		sleep(2);
 
-// 		key_pair* keys = test_coord.query_handled_requests();
+		key_pair* keys = test_coord.query_handled_requests();
 
-// 		for (int i=0;i<10;i++)
-// 		{
-// 			std::cout << keys[i].request_shm_key << '\n';
-// 			std::cout << keys[i].response_shm_key << '\n';
-// 		}
+		for (int i=0;i<10;i++)
+		{
+			std::cout << keys[i].request_shm_key << '\n';
+			std::cout << keys[i].response_shm_key << '\n';
+		}
 
-//     } 
-//     else { //CHILD PROCESS
+    } 
+    else { //CHILD PROCESS
 
-// 		shared_memory_region coord_region(1, 200, create_flag);
-// 		coord_region.create();
+		shared_memory_region coord_region(1, 200, create_flag);
+		coord_region.create();
 
-// 		coord test_coord;
-// 		test_coord.attach(&coord_region);
+		coord test_coord;
+		test_coord.attach(&coord_region);
 
-// 		key_pair keys = test_coord.try_request_keys(c_pid);
+		key_pair keys = test_coord.try_request_keys(c_pid);
+		std::cout << keys.request_shm_key << '\n';
+		std::cout << keys.response_shm_key << '\n';
+
+
+		// key_pair* keys = test_coord.query_handled_requests();
+
+		// for (int i=0;i<10;i++)
+		// {
+		// 	std::cout << keys[i].request_shm_key << '\n';
+		// 	std::cout << keys[i].response_shm_key << '\n';
+		// }
+
 		
-//     } 
+    } 
 
-// }
+}
 
 
 
 int main() {
 	// data_race_test();
-	child_process_test();
-	// coord_test();
+	// child_process_test();
+	coord_test();
 
 	return 0;
 
