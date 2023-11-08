506cd64c85549cb2cd80c393499fe684124ffc4e
Esteban Ramos <estebanramo@gmail.com>
2023-11-08 11:54:06 -0800

    cleanup

--- src/rpc.h
+++ src/rpc.h
@@ -35,12 +35,11 @@ struct server_context {
  * the address to resolve for a unique shm region key in which
  * a coord request for a new queue pair can be made. This can 
  * fail due to the server not having initialized its coord region,
- * no available reserve slots. 
+ * no available reserve slots,etc 
  * 
- * // Client ID: Client Address + Nonce.
- * // It is an identifier IP and Sequence Number 
  * 
- * @param address[IN] Unique string to identify server address,
+ * @param source_addr[IN] source_addr: Client Address + Nonce.
+ * @param destination_addr[IN] Unique string to identify server address,
  * usually IP/Port pair
  * @return connection* If NULL, no connection was achieved
  */
@@ -48,14 +47,14 @@ queue_pair* open(char* source_addr, char* destination_addr);
 
 
 /**
- * @brief Client writes to request queue in given connection.
+ * @brief Client writes to the request queue in given connection.
  * 
  * TODO: 
  * - What if queue full? return 0 for bytes written
  * 
+ * @param conn[IN] queues initialized with server
  * @param buf[IN] data buf to be written
  * @param size[IN] size of buf
- * @param conn[IN] queues initialized with server
  * @return ssize_t -1 for error, >0 for bytes written
  */
 ssize_t send_rpc(queue_pair* conn, const void *buf, size_t size);
@@ -66,14 +65,14 @@ ssize_t send_rpc(queue_pair* conn, const void *buf, size_t size);
  * message queue.
  * 
  * TODO: How do we keep track in the message queue how much has been written?
- * 
+ * TODO: How can we make the implementation zero-copy?
  * 
  * @param conn [IN] queues initialized with server
  * @param buf [IN] data buf to be copied to
  * @param size [IN] size of buf
  * @return ssize_t How much read, if 0 EOF. Only removed from queue once fully read. 
  */
-ssize_t receive_rpc(queue_pair* conn, void** buf, size_t size);
+ssize_t receive_buf(queue_pair* conn, void** buf, size_t size);
 
 
 /**
@@ -87,8 +86,10 @@ ssize_t receive_rpc(queue_pair* conn, void** buf, size_t size);
  * - malformed
  * - ERRNO
  * 
- * @param conn[IN] connection initialized with server
- */
+ * @param source_addr[IN] source_addr: Client Address + Nonce.
+ * @param destination_addr[IN] Unique string to identify server address,
+ * usually IP/Port pair 
+*/
 void close(char* source_addr, char* destination_addr);
 
 
@@ -96,9 +97,8 @@ void close(char* source_addr, char* destination_addr);
 //SERVER
 /**
  * @brief Server will instantiate necessary coord memory regions. 
- * Once instantiate server can begin servicing client requests
- * 
- * 1. Invoked at instantiation of server. Only once per server
+ * Once instantiate server can begin servicing client requests. 
+ * Only once per server
  * 
  * @param source_addr[IN] Unique string to identify desired server address,
  * usually IP/Port pair
@@ -109,31 +109,34 @@ server_context* register_server(char* source_addr);
 
 
 /**
- * @brief
- * 
- * Returns a queue pair corresponding to a client who requested it.
- * Will only service one client per call? In round robin, cycles
- * through currently servicing clients and returns queue pair accordingly.
- * This differs from the usual listen/accept pattern
- * 
- * 1. Needs to be periodically run to service 
- * client requests: open, close
- * 
- * @param handler[IN/OUT] server client connection handler
- */
-queue_pair* listen(server_context* handler);
-
-
-/**
  * @brief 
  * 
  * Service client requests, allocate communication queues, and
- * signal to clients their details. Modifies coord space.
+ * signal to clients their queue details. Modifies coord space.
+ *  * 
+ * 1. Needs to be periodically run to service 
+ * client requests: open, close
+ * 2. Service client requests in order of arrival. 
  * 
  * @param handler [IN/OUT] server client connection handler
+ * 
+ */
+void manage_pool(server_context* handler);
+
+
+/**
+ * @brief
+ * 
+ * Returns a queue pair corresponding to a client who requested it.
+ * Will only service one client per call? In round robin fashion, cycles
+ * through currently servicing clients and returns queue pair accordingly.
+ * This differs from the usual listen/accept pattern.
+ * 
+ * 
+ * @param handler[IN/OUT] server client connection handler
  * @return queue_pair 
  */
-int manage_conn(server_context* handler);
+queue_pair* accept(server_context* handler);
 
 
 /**
@@ -157,29 +160,7 @@ ssize_t send_rpc(queue_pair* client, const void *buf, size_t size);
  * @param size [IN] size of buf
  * @return ssize_t How much read, if 0 EOF. Only removed from queue once fully read. 
  */
-ssize_t receive_rpc(queue_pair* client, void** buf, size_t size);
-
-/**
- * @brief This method is a generic read to read a single incoming request
- * from any client. The order of these reads will depend on a queuing algorithm,
- * ie. round robin, fifo, etc... We can choose to transparent the queuing methodology
- * for whatever consuming system that uses this.
- * TODO: Question: Should read be preallocated? Message size will be determined
- * by the client.
- * TODO: What is the correct way to read incoming messages? There could be 
- * a variety of queuing algorithms we could choose. There is an interesting 
- * opportunity for per connection load balancing at the server level to occur.
- * But for not an algorithm that ensures fairness would be best. Round robin seems
- * like the best and easiest for now, as other algorithms would require greater visibility
- * into the state of the queues. Round robin represents different behavior than how
- * a typical socket read would work, as sockets order messages by delivery (i think). 
- * 
- * @param buf [OUT] data buf for data to be read into
- * @param size [OUT] size of read to be performed
- * @param handler [IN] server client connection handler
- * @return ssize_t -1 for error, >0 for bytes read
- */
-void* receive_next_rpc(size_t *size, server_context* handler, queue_pair** client);
+ssize_t receive_buf(queue_pair* client, void** buf, size_t size);
 
 
 /**
