9f99e0988526ad65bfc5f415dce254c8267fc6cf
Esteban Ramos <estebanramo@gmail.com>
2023-11-10 14:32:39 -0800

    goals, basic setup

---
+++ tests_perf/rpc/main.c
@@ -0,0 +1,22 @@
+// #include "../../src/queue.h"
+//Currently commented out as it has not been updated
+#include <stdio.h>
+
+#include "tests.c"
+
+
+
+/* Run client and server processes from same program distinguished by different arguments
+Goals:
+- Measure "echo" latency
+- Measure throughput as inverse of latency as it is single threaded 
+- Tune payload size
+- Tune queue size
+*/ 
+
+
+int main(int argc, char *argv[])
+{
+
+
+}
---
+++ tests_perf/rpc/tests.c
@@ -0,0 +1,13 @@
+
+void test_success_print(const char *message) {
+    printf("SUCCESS:%s\n", message);
+}
+
+
+void test_failed_print(const char *message) {
+    printf("FAILED:%s\n", message);
+}
+
+void tests_run_all(void){
+
+}
