020f81ba53622fd47cd8c9d088fb2b421dbb628d
David Cheng <davidcheng34321@gmail.com>
2023-11-15 18:16:17 -0500

    initial tests for rpc

---
+++ tests/rpc/tests.c
@@ -0,0 +1,29 @@
+#include "../../src/rpc.h"
+
+#include <assert.h>
+
+void test_success_print(const char *message) {
+    printf("SUCCESS:%s\n", message);
+}
+
+
+void test_failed_print(const char *message) {
+    printf("FAILED:%s\n", message);
+}
+
+void test_register_server_shutdown() {
+    server_context* sc = register_server("common_name");
+    int err = (sc == NULL);
+    sleep(4);
+    shutdown(sc);
+
+    if (err) {
+        test_failed_print(__func__);
+    } else {
+        test_success_print(__func__);
+    }
+}
+
+void tests_run_all() {
+//    test_register_server_shutdown();
+}
