e4df03d2c9bbbd0b09461e1d52d737dd5f6161a7
Esteban Ramos <estebanramo@gmail.com>
2024-03-05 07:59:47 +0000

    initial simulation setup

---
+++ simulation/include/simulation/Constants.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace simulation
+{
+  const static int numRuns = 30;
+}  /* namespace */
---
+++ simulation/include/simulation/ExperimentalRun.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace simulation
+{
+    class ExperimentalRun
+    {
+    public:
+        virtual ~ExperimentalRun()
+        {}
+
+        virtual void process() = 0;
+
+        void run();
+
+    protected:
+        virtual void setUp()
+        {}
+
+        virtual void tearDown()
+        {}
+
+        std::string getClassName();
+
+        std::string getFullyQualifiedClassName();
+    };
+}  /* namespace */
