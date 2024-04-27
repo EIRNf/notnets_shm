13ec547615a6c955950fa395b379de06c4e47179
Esteban Ramos <estebanramo@gmail.com>
2024-04-27 08:43:09 +0000

    iperf run script

---
+++ experiments/viz/iperf_exps/iperf_run_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+### Run Parameters
+###     iperf -c 127.0.0.1 --port 5001 -P 12 -t 30 -l 128 -N -e --trip-times 
+host=127.0.0.1
+port=5001
+payloadBufSize=4
+numClients=25
+runtime=90
+message_size=128
+num_burst_messages=1
+bounceback_period=0
+
+
+### Run Server
+iperf -s  &
+
+### Run Client
+
+for num in $(seq 1 $numClients)
+do
+    filename="iperf_${num}.csv"
+    iperf -c $host --port $port -P $num -t $runtime -l $message_size  -N -e --bounceback=$num_burst_messages --bounceback-period=$bounceback_period > $filename
+done
